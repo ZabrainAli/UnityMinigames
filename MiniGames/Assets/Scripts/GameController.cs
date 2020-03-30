68df0cf278df9bc15d9dfc63f33c8e030a8ffccf
Zabrain Ali <aliz8@mcmaster.ca>
2020-03-30 16:13:11 -0400

    Adding win screen, pause screen, and game over scree. Also made some changes to game level such as making coins dissapear on contact and adding collider to water so the player does not go through the water and out of the level

---
+++ MiniGames/Assets/Scripts/GameController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+	
+	public GameObject coin;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ MiniGames/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+	public GameObject Coin;
+	public GameObject Coin1;
+	public GameObject Coin2;
+	public GameObject Coin3;
+	public GameObject Coin4;
+	public GameObject Coin5;
+    public GameObject Coin6;
+    public GameObject Coin7;
+	public GameObject EndCoin;
+
+    void OnCollisionEnter2D(UnityEngine.Collision2D collisionInfo)
+    {
+    	
+    	if (collisionInfo.gameObject.tag == "Coin")
+    	{
+    		Coin.SetActive(false);
+    	}
+
+    	if (collisionInfo.gameObject.tag == "Coin1")
+    	{
+    		Coin1.SetActive(false);
+    	}
+
+    	if (collisionInfo.gameObject.tag == "Coin2")
+    	{
+    		Coin2.SetActive(false);
+    	}
+
+    	if (collisionInfo.gameObject.tag == "Coin3")
+    	{
+    		Coin3.SetActive(false);
+    	}
+
+    	if (collisionInfo.gameObject.tag == "Coin4")
+    	{
+    		Coin4.SetActive(false);
+    	}
+
+    	if (collisionInfo.gameObject.tag == "Coin5")
+    	{
+    		Coin5.SetActive(false);
+    	}
+
+        if (collisionInfo.gameObject.tag == "Coin6")
+        {
+            Coin6.SetActive(false);
+        }
+
+        if (collisionInfo.gameObject.tag == "Coin7")
+        {
+            Coin7.SetActive(false);
+        }
+
+    	if (collisionInfo.gameObject.tag == "EndCoin")
+    	{
+    		EndCoin.SetActive(false);
+    	}
+    }
+}
